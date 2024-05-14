ba7b8f7ee41b68a82cfad3a77b10e48e9f5b4a53
Ana Laura Andrade <143561088+nalaurandrade@users.noreply.github.com>
2024-05-13 21:40:39 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Cliente
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+
+        List<Telefone> ListaDeTelefone = new List<Telefone>();
+
+        public void AdicionarTelefone(Telefone tel)
+        {
+            ListaDeTelefone.Add(tel);
+        }
+
+        public void ImprimirTelefones()
+        { 
+            foreach (Telefone t in ListaDeTelefone) 
+            {
+                Console.WriteLine(t.ddd + "-" + t.numero);
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp1;
+
+Telefone _tel = new Telefone();
+_tel.ddd = "34";
+_tel.numero = "99950505";
+
+Cliente _cliente = new Cliente();
+_cliente.Nome = "Jose";
+_cliente.Codigo = 5050;
+_cliente.AdicionarTelefone(_tel);
+
+_tel.ddd = "34";
+_tel.numero = "9090";
+_cliente.AdicionarTelefone(_tel);
+
+_tel.ddd = "35";
+_tel.numero = "9091";
+_cliente.AdicionarTelefone(_tel);
+
+_cliente.ImprimirTelefones();
